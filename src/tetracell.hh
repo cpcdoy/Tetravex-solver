bf12fca40a154dc05dca89d7726fc19258686fe0
Axel Mendoza <axel.mendoza@epita.fr>
2018-01-03 15:16:09 +0100

    add tetracell class

---
+++ src/tetracell.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+class TetraCell
+{
+    public:
+        TetraCell(int up, int down, int right, left)
+        {
+            this.up = up;
+            this.down = down;
+            this.right = right;
+            this.left = left;
+        }
+        int up;
+        int down;
+        int right;
+        int left;
+};
